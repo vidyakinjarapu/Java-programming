aa06cd8460a5a37599a214f1a5645aec345abc42
vidyakinjarapu <vidya5594@gmail.com>
2021-12-22 12:10:06 +0530

    Add Part7

--- part06-Part06_13.Exercises/src/main/java/ExerciseManagement.java
+++ part06-Part06_13.Exercises/src/main/java/ExerciseManagement.java
@@ -1,4 +1,31 @@
+import java.util.ArrayList;
 
 public class ExerciseManagement {
-
+    private ArrayList<String> exercises;
+    private ArrayList<String> completedExcercises;
+    
+    public ExerciseManagement(){
+        this.exercises = new ArrayList<>();
+        this.completedExcercises = new ArrayList<>();
+    }  
+    
+    public ArrayList<String> exerciseList(){
+        return this.exercises;
+    }
+    
+//    public ArrayList<String> completedExcercises(){
+//        return this.completedExcercises;
+//    }
+//    
+    public void add(String exercise){
+        this.exercises.add(exercise);
+    }
+    
+    public void markAsCompleted(String exercise){
+        this.completedExcercises.add(exercise);
+    }
+    
+    public boolean isCompleted(String exercise){
+        return this.completedExcercises.contains(exercise);
+    }    
 }
--- part06-Part06_13.Exercises/src/test/java/ExerciseManagementTest.java
+++ part06-Part06_13.Exercises/src/test/java/ExerciseManagementTest.java
@@ -1,6 +1,45 @@
-
-
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.Before;
 
 public class ExerciseManagementTest {
-
+    private ExerciseManagement mgnt;
+    
+    @Before 
+    public void initialize(){
+        mgnt = new ExerciseManagement();
+    }
+    
+    @Test
+    public void excerciseListEmptyAtBeginning(){
+//        ExerciseManagement mgnt = new ExerciseManagement();
+        assertEquals(0, mgnt.exerciseList().size());
+    }
+    
+    @Test
+    public void addingExcerciseGrowsByOne(){
+//        ExerciseManagement mgnt = new ExerciseManagement();
+        mgnt.add("Write a test");
+        assertEquals(1, mgnt.exerciseList().size());
+    }
+    
+    @Test
+    public void addedExcerciseIsInList(){
+        mgnt.add("Sample ex");
+        assertTrue(mgnt.exerciseList().contains("Sample ex"));
+    }
+    
+    @Test
+    public void exerciseCanBeMarkedAsCompleted(){
+        mgnt.add("new ex");
+        mgnt.markAsCompleted("new ex");
+        assertTrue(mgnt.isCompleted("new ex"));
+    }
+    
+    @Test
+    public void ifNotMarkedCompletedIsNotCompleted(){
+        mgnt.add("new ex");
+        mgnt.markAsCompleted("new ex");
+        assertFalse(mgnt.isCompleted("new ex"));
+    } 
 }

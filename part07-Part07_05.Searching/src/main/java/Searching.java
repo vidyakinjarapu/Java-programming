4439f415fae2e12e060b6534b3b501737d9be264
vidyakinjarapu <vidya5594@gmail.com>
2021-12-29 00:07:36 +0530

    Algorithms

--- part07-Part07_05.Searching/src/main/java/Searching.java
+++ part07-Part07_05.Searching/src/main/java/Searching.java
@@ -8,8 +8,10 @@ public class Searching {
         // The program below is meant for testing the search algorithms you'll write
         Scanner scanner = new Scanner(System.in);
         ArrayList<Book> books = new ArrayList<>();
+        
         System.out.println("How many books to create?");
         int numberOfBooks = Integer.valueOf(scanner.nextLine());
+        
         for (int i = 0; i < numberOfBooks; i++) {
             books.add(new Book(i, "name for the book " + i));
         }
@@ -18,10 +20,13 @@ public class Searching {
         int idToSearchFor = Integer.valueOf(scanner.nextLine());
 
         System.out.println("");
+        
         System.out.println("Searching with linear search:");
+        
         long start = System.currentTimeMillis();
-        int linearSearchId = linearSearch(books, idToSearchFor);
+        int linearSearchId = linearSearch(books, idToSearchFor);       
         System.out.println("The search took " + (System.currentTimeMillis() - start) + " milliseconds.");
+        
         if (linearSearchId < 0) {
             System.out.println("Book not found");
         } else {
@@ -44,11 +49,29 @@ public class Searching {
     }
 
     public static int linearSearch(ArrayList<Book> books, int searchedId) {
+        for(int i = 0; i < books.size(); i++){
+            if(searchedId == books.get(i).getId()){
+                return i;
+            }
+        }
         return -1;
     }
 
-    public static int binarySearch(ArrayList<Book> books, long searchedId) {
+    public static int binarySearch(ArrayList<Book> books, long searchedId){
+        int start = 0;
+        int end = books.size()-1;
+        
+        while(start <= end){           
+            int middle = (start + end)/ 2;
+            
+            if(searchedId == books.get(middle).getId()){
+                return middle;
+            } else if(searchedId > books.get(middle).getId()){
+                start = middle + 1;
+            } else {
+                end = middle - 1;
+            } 
+        }
         return -1;
     }
 }
-
